b99f3f11434140d9555a6603680407b1191843a6
Mikhail Sarnov <kpblm2@gmail.com>
2024-04-23 15:52:37 +0200

    updated index.ts

--- aws-ec2/index.ts
+++ aws-ec2/index.ts
@@ -1,35 +1,78 @@
-import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
+import * as pulumi from "@pulumi/pulumi";
 
-// Create a new VPC
 const vpc = new aws.ec2.Vpc("mik-pulumi-vpc", {
-    cidrBlock: "10.0.0.0/16",
+  cidrBlock: "10.0.0.0/16",
+  enableDnsHostnames: true,
+  enableDnsSupport: true,
 });
 
-// Create a public subnet within the VPC
-const subnet = new aws.ec2.Subnet("mik-pulumi-subnet", {
-    vpcId: vpc.id,
-    cidrBlock: "10.0.1.0/24",
-    mapPublicIpOnLaunch: true,
+const publicSubnet = new aws.ec2.Subnet("mik-pulumi-subnet", {
+  vpcId: vpc.id,
+  cidrBlock: "10.0.1.0/24",
+  availabilityZone: "eu-central-1a",
+  mapPublicIpOnLaunch: true,
 });
 
-// Configure the EC2 instance with UserData
-const userDataScript = pulumi.interpolate`#!/bin/bash
-echo "Hello, World!" > index.html
-nohup python -m SimpleHTTPServer 80 &`;
+const ig = new aws.ec2.InternetGateway("ig", {
+  vpcId: vpc.id,
+});
 
-
-// Create an EC2 instance in the public subnet
-const instance = new aws.ec2.Instance("mik-pulumi-instance", {
-    instanceType: "t2.micro",
-    vpcSecurityGroupIds: [vpc.defaultSecurityGroupId],
-    subnetId: subnet.id,
-    ami: "ami-0f673487d7e5f89ca",
-    userData: userDataScript,
-    tags: {
-        Name: "podinfo-instance",
+const rt = new aws.ec2.RouteTable("rt", {
+  routes: [
+    {
+      cidrBlock: "0.0.0.0/0",
+      gatewayId: ig.id,
     },
+  ],
+  vpcId: vpc.id,
 });
 
-// Export the public IP address of the EC2 instance
-export const instancePublicIp = instance.publicIp;
+new aws.ec2.RouteTableAssociation("rta", {
+  routeTableId: rt.id,
+  subnetId: publicSubnet.id,
+});
+
+const securityGroup = new aws.ec2.SecurityGroup("web-sg", {
+  vpcId: vpc.id,
+  ingress: [
+    {
+      protocol: "tcp",
+      fromPort: 80,
+      toPort: 80,
+      cidrBlocks: ["0.0.0.0/0"],
+    },
+    {
+      fromPort: 22,
+      toPort: 22,
+      protocol: "tcp",
+      cidrBlocks: ["0.0.0.0/0"],
+    },
+  ],
+  egress: [
+    {
+      fromPort: 0,
+      toPort: 0,
+      protocol: "-1",
+      cidrBlocks: ["0.0.0.0/0"],
+    },
+  ],
+});
+
+const instance = new aws.ec2.Instance("podinfo-instance", {
+  instanceType: "t2.micro",
+  vpcSecurityGroupIds: [securityGroup.id],
+  subnetId: publicSubnet.id,
+  ami: "ami-0f673487d7e5f89ca",
+  tags: {
+    Name: "podinfo-instance",
+  },
+  userData: pulumi.interpolate`#!/bin/bash
+        sudo yum install -y docker
+        sudo systemctl start docker
+        sudo systemctl enable docker
+        sudo docker run -d -p 80:9898 stefanprodan/podinfo
+    `,
+});
+
+export const publicIp = instance.publicIp;
